de1652b28c6a5a4ded105dc6fba3e7c92b04c341
Nova Guo <qiaochug@seas.upenn.edu>
2017-02-25 18:42:44 -0500

    survey up

---
+++ js/survey.display.js
@@ -0,0 +1,28 @@
+Survey.Survey.cssType = "bootstrap";
+   var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
+  { name:"page1", questions: [ 
+      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
+      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
+   ]},
+  { name: "page2", questions: [
+    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
+    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
+  { name: "page3",questions: [
+    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+ ]
+}
+var survey = new Survey.Model(surveyJSON);
+$("#surveyContainer").Survey({
+    model:survey,
+    onComplete:sendDataToServer
+});
+function sendDataToServer(survey) {
+  var resultAsString = JSON.stringify(survey.data);
+  alert(resultAsString); //send Ajax request to your web server.
+}
+
+// survey.onComplete.add(function (sender) {
+//     var mySurvey = sender;
+//     var surveyData = sender.data;
+//     alert(surveyData);
+// });
