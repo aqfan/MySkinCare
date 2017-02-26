ad07764b61542e641c08d2f37a3e39e1a407e64d
Nova Guo <qiaochug@seas.upenn.edu>
2017-02-25 19:36:20 -0500

    survey has result

--- js/survey.display.js
+++ js/survey.display.js
@@ -1,24 +1,125 @@
 Survey.Survey.cssType = "bootstrap";
-   var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-  { name:"page1", questions: [ 
-      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
-   ]},
-  { name: "page2", questions: [
-    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
-  { name: "page3",questions: [
-    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+   var surveyJSON = { title: "Let us help you personalize your skin care routine",  pages: [
+  {
+   name: "page1",
+   questions: [
+    {
+     type: "dropdown",
+     choices: [
+      {
+       value: "1",
+       text: "Under 20"
+      },
+      {
+       value: "2",
+       text: "20 - 30 "
+      },
+      {
+       value: "3",
+       text: "30 - 40 "
+      }
+     ],
+     name: "age",
+     title:"How old are you?"
+    },
+    {
+     type: "dropdown",
+     choices: [
+      {
+       value: "1",
+       text: "Oily, Acne prone"
+      },
+      {
+       value: "2",
+       text: "Dry ,dehydrated"
+      },
+      {
+       value: "5",
+       text: "Wrinkles"
+      },
+      {
+       value: "6",
+       text: "NONE!"
+      }
+     ],
+     name:"problem",
+     title: "What skin problem do you have?"
+    },
+    {
+     type: "dropdown",
+     choices: [
+      {
+       value: "1",
+       text: "Yes"
+      },
+      {
+       value: "2",
+       text: "No"
+      }
+     ],
+     name:"moisturizer",
+     title: "Do you have a moisturizer?"
+    },
+    {
+     type: "dropdown",
+     choices: [
+      {
+       value: "1",
+       text: "Yes! "
+      },
+      {
+       value: "2",
+       text: "No."
+      }
+     ],
+     name:"serum",
+     title: "Do you have serum?"
+    },
+    {
+     type: "dropdown",
+     choices: [
+      {
+       value: "1",
+       text: "Yes! Hydration Mask"
+      },
+      {
+       value: "2",
+       text: "No. But I have dry skin. "
+      },
+      {
+       value: "3",
+       text: "Yes! Purifying Mask"
+      },
+      {
+       value: "4",
+       text: "No! But I have oily skin. "
+      }
+     ],
+     name:"mask",
+     title: "Do you have a mask?"
+    }
+   ]
+  }
  ]
 }
+
 var survey = new Survey.Model(surveyJSON);
 $("#surveyContainer").Survey({
     model:survey,
     onComplete:sendDataToServer
 });
 function sendDataToServer(survey) {
-  var resultAsString = JSON.stringify(survey.data);
-  alert(resultAsString); //send Ajax request to your web server.
+  var surveyObjects = JSON.parse(JSON.stringify(survey.data));
+  // alert(JSON.stringify(survey.data)+surveyObjects.age
+  // +surveyObjects.problem+surveyObjects.moisturizer
+  // +surveyObjects.serum+surveyObjects.mask);
+  var age = surveyObjects.age;
+  var problem = surveyObjects.problem;
+  var moisturizer = surveyObjects.moisturizer;
+  var serum = surveyObjects.serum;
+  var mask = surveyObjects.mask;
+  alert(JSON.stringify(survey.data)+age
+  +problem+moisturizer+serum+mask);
 }
 
 // survey.onComplete.add(function (sender) {
